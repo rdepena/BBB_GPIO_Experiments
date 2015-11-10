679a5ec2e24725913b6040a14f8d2082276c6bf1
Ricardo <ricardo.depena@gmail.com>
2015-11-10 00:13:08 -0500

    added digital read loop for a button on pin 44

--- test.c
+++ test.c
@@ -1,29 +1,48 @@
 #include <stdio.h>
 #include "src/bbbgpio.h"
 
-#define ledPin 60
+#define blueLed 60
+#define whiteLed 66
+#define buttonPin 44
 
 int main(void) {
 
-    pinMode(ledPin, OUTPUT);
+    int pinValue = 0,
+        buttonValue = 0;
 
-    digitalWrite(ledPin, HIGH);
+    pinMode(blueLed, OUTPUT);
+    pinMode(whiteLed, OUTPUT);
+    pinMode(buttonPin, INPUT);
 
-    int pinValue = digitalRead(ledPin);
+
+    digitalRead(blueLed);
     if (pinValue == HIGH) {
         printf("pin value at HIGH\n");
     }
 
 
+    while((buttonValue = digitalRead(buttonPin)) == 0) {
+        digitalWrite(blueLed, LOW);
+        digitalWrite(whiteLed, HIGH);
+    }
+
+    printf("button value: %d\n", buttonValue);
+
+    digitalWrite(blueLed, HIGH);
+    digitalWrite(whiteLed, LOW);
+
     printf("turn it off\n");
     getchar();
 
-    digitalWrite(ledPin, LOW);
+    digitalWrite(blueLed, LOW);
+    digitalWrite(whiteLed, LOW);
 
-    pinValue = digitalRead(ledPin);
+    pinValue = digitalRead(blueLed);
     if (pinValue == LOW) {
         printf("pin value at LOW\n");
     }
 
-    unExportPin(ledPin);
+    unExportPin(blueLed);
+    unExportPin(whiteLed);
+
 }
